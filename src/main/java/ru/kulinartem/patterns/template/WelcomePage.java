69ec70aad440800b814ca83d0f6df9e32ef9a905
Artem Kulin <kulinartem@gmail.com>
2022-04-17 15:29:49 +0300

    Add Template pattern

---
+++ src/main/java/ru/kulinartem/patterns/template/WelcomePage.java
@@ -0,0 +1,9 @@
+package ru.kulinartem.patterns.template;
+
+public class WelcomePage extends WebsiteTemplate {
+
+    @Override
+    public void showPageContent() {
+        System.out.println("Welcome page content");
+    }
+}
